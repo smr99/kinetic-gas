03627b688018971c6dd4abb5ddd6fe5a694a8cea
Steven M. Robbins <smr@sumost.ca>
2014-04-27 16:20:29 -0500

    Change creating a particle from "is touched" polling to a touch down event to avoid creating a stream of particles at the screen update rate. Move methods MakeWall() and MakeParticle() from screen to game class.

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,26 +1,18 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
 
 public class GameScreen implements Screen 
 {
-	private final MyGdxGame game;
+	final MyGdxGame game;
 	private final OrthographicCamera camera = new OrthographicCamera();
 
-	private final World world = new World(new Vector2(0,-0), true);
 	private final Box2DDebugRenderer debugRenderer = new Box2DDebugRenderer();
 	
 	private final Vector3 touchPos = new Vector3();
@@ -31,62 +23,25 @@ public class GameScreen implements Screen
 		game = g;
 		camera.setToOrtho(false, MyGdxGame.WIDTH, MyGdxGame.HEIGHT);
 		
-		MakeWall(MyGdxGame.WIDTH/2, 5,                    MyGdxGame.WIDTH, 10);
-		MakeWall(MyGdxGame.WIDTH/2, MyGdxGame.HEIGHT - 5, MyGdxGame.WIDTH, 10);
-		MakeWall(5,                   MyGdxGame.HEIGHT/2, 10, MyGdxGame.HEIGHT);
-		MakeWall(MyGdxGame.WIDTH - 5, MyGdxGame.HEIGHT/2, 10, MyGdxGame.HEIGHT);
-		MakeParticle(100, 300);
+		game.MakeWall(MyGdxGame.WIDTH/2, 5, MyGdxGame.WIDTH,                    10);
+		game.MakeWall(MyGdxGame.WIDTH/2, MyGdxGame.HEIGHT - 5, MyGdxGame.WIDTH, 10);
+		game.MakeWall(5, MyGdxGame.HEIGHT/2,                   10, MyGdxGame.HEIGHT);
+		game.MakeWall(MyGdxGame.WIDTH - 5, MyGdxGame.HEIGHT/2, 10, MyGdxGame.HEIGHT);
+		game.MakeParticle(100, 300);
+		
+		Gdx.input.setInputProcessor(new InputAdapter()
+		{
+			public boolean touchDown(int x, int y, int pointer, int button)
+			{
+				touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+	        	camera.unproject(touchPos);
+	        	game.MakeParticle(touchPos.x, touchPos.y);
+				return true;
+			}
+		});
+		
 	}
 	
-	void MakeWall(float cx, float cy, float hx, float hy)
-	{
-		// Create our body definition
-		BodyDef groundBodyDef = new BodyDef();  
-		groundBodyDef.position.set(new Vector2(cx, cy));  
-
-		// Create a body from the definition and add it to the world
-		Body groundBody = world.createBody(groundBodyDef);  
-
-		// Create a polygon shape
-		PolygonShape groundBox = new PolygonShape();  
-		// Set the polygon shape as a box which is the size of our view port and 20 high
-		// (setAsBox takes half-width and half-height as arguments)
-		groundBox.setAsBox(hx, hy);
-		groundBody.createFixture(groundBox, 0.0f); 
-
-		groundBox.dispose();
-	}
-	
-	void MakeParticle(float x, float y)
-	{
-		// First we create a body definition
-		BodyDef bodyDef = new BodyDef();
-		bodyDef.type = BodyType.DynamicBody;
-		bodyDef.position.set(x, y);
-
-		// Create our body in the world using our body definition
-		Body body = world.createBody(bodyDef);
-		body.applyLinearImpulse(30, -30, 0, 0, true);
-
-		// Create a circle shape and set its radius to 6
-		CircleShape circle = new CircleShape();
-		circle.setRadius(6f);
-
-		// Create a fixture definition to apply our shape to
-		FixtureDef fixtureDef = new FixtureDef();
-		fixtureDef.shape = circle;
-		fixtureDef.density = 0.5f; 
-		fixtureDef.friction = 0f;
-		fixtureDef.restitution = 1f; // Completely elastic
-
-		// Create our fixture and attach it to the body
-		body.createFixture(fixtureDef);
-
-		// Remember to dispose of any shapes after you're done with them!
-		// BodyDef and FixtureDef don't need disposing, but shapes do.
-		circle.dispose();
-	}
-
 	@Override
 	public void render(float delta) 
 	{
@@ -94,16 +49,9 @@ public class GameScreen implements Screen
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();        
-        debugRenderer.render(world, camera.combined);
-
-        if (Gdx.input.isTouched())
-        {
-        	touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-        	camera.unproject(touchPos);
-        	MakeParticle(touchPos.x, touchPos.y);
-        }
+        debugRenderer.render(game.world, camera.combined);
         
-		world.step(1/60f, 6, 2);
+		game.world.step(1/60f, 6, 2);
 	}
 
 	@Override
