9c85d02e1d838f7d6367ff18cdd1939f49d46bc3
Steven M. Robbins <smr@sumost.ca>
2014-11-08 22:42:32 -0600

    Restore the title, width, and height settings.

--- desktop/src/ca/sumost/kinetic/desktop/DesktopLauncher.java
+++ desktop/src/ca/sumost/kinetic/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import ca.sumost.kinetic.KineticTheoryGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Kinetic Gas Theory";
+		config.width = KineticTheoryGame.VIEWPORT_WIDTH;
+		config.height = KineticTheoryGame.VIEWPORT_HEIGHT;
 		new LwjglApplication(new KineticTheoryGame(), config);
 	}
 }
