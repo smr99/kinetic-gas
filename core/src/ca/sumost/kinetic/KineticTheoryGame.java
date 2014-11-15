eb260b9ab9d42e996b2cd6e632d0f3e0780168a7
Steven M. Robbins <smr@sumost.ca>
2014-11-15 11:41:10 -0600

    Use Skin to render the MainMenuScreen.  New asset fiels are from the libgdx tests repository.

--- core/src/ca/sumost/kinetic/KineticTheoryGame.java
+++ core/src/ca/sumost/kinetic/KineticTheoryGame.java
@@ -4,27 +4,34 @@ package ca.sumost.kinetic;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Version;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class KineticTheoryGame extends Game
 {
 	static public final int VIEWPORT_WIDTH = 2560/2;
 	static public final int VIEWPORT_HEIGHT = 1600/2;
 
-	public SpriteBatch batch;
-	public BitmapFont font;
+	
+	public SpriteBatch batch;//@deprecated
+	public BitmapFont font;//@deprecated
+	
 	public final World world = new World(new Vector2(0, 0), true);
+	private Skin mSkin;
 	
 	@Override
 	public void create () 
 	{
 		Gdx.app.log("Startup", "libGDX version " + Version.VERSION);
 		
+		mSkin = makeSkin();
 		batch = new SpriteBatch();
-		font = new BitmapFont();
+		font = mSkin.getFont("default-font");
 		
 		Gdx.graphics.setContinuousRendering(false);
 		Gdx.graphics.requestRendering();
@@ -38,4 +45,18 @@ public class KineticTheoryGame extends Game
 		batch.dispose();
 		font.dispose();
 	}
+
+	private static Skin makeSkin() 
+	{
+		FileHandle skinFile = Gdx.files.internal("data/uiskin.json");
+		TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("data/uiskin.atlas"));
+		Skin skin = new Skin(skinFile, atlas);
+		return skin;
+	}
+
+	public final Skin getSkin() 
+	{
+		return mSkin;
+	}
+	
 }
--- core/src/ca/sumost/kinetic/MainMenuScreen.java
+++ core/src/ca/sumost/kinetic/MainMenuScreen.java
@@ -3,33 +3,49 @@ package ca.sumost.kinetic;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenuScreen extends ScreenAdapter //implements Screen 
 {
 	private final KineticTheoryGame game;
-		
-	private final OrthographicCamera camera = new OrthographicCamera();
-		
+	private final Stage mStage;
+	private final Table mTable;
+	
 	public MainMenuScreen(final KineticTheoryGame passed_game) 
 	{
 		game = passed_game;	
-		camera.setToOrtho(false);
+		mStage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(mStage);
+		
+		mTable = new Table();
+		mTable.setFillParent(true);
+		mStage.addActor(mTable);
+		
+		initializeTable();
 	}
 		
+	private void initializeTable()
+	{
+		Skin skin = game.getSkin();
+		
+	    Table table = mTable;
+	    table.add(new Label("Welcome to Kinetic Theory!!", skin));
+	    table.row();
+	    table.add(new Label("Tap anywhere to begin!", skin));
+	}
+
 	@Override
 	public void render(float delta) 
 	{
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-			
-		camera.update();
-		game.batch.setProjectionMatrix(camera.combined);
-			
-		game.batch.begin();
-		game.font.draw(game.batch, "Welcome to Kinetic Theory!!", 100, 150);
-		game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
-		game.batch.end();
+		
+		mStage.act(delta);
+		mStage.draw();
 			
 		// If player activates the game, dispose of this menu.
 		if (Gdx.input.isTouched()) 
@@ -39,5 +55,13 @@ public class MainMenuScreen extends ScreenAdapter //implements Screen
 			dispose();
 		}
 	}
-
+	public void resize (int width, int height) 
+	{
+	    mStage.getViewport().update(width, height, true);
+	}
+	
+	public void dispose() 
+	{
+	    mStage.dispose();
+	}
 }
